8ed0f7d98a603e5f7576e9a52ce9cf5bbb040bd5
Costa Ilisescu <coste_ilisescu@yahoo.com>
2023-09-30 10:11:28 +0100

    Add HomeServices component to Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeHome from "./Components/HomeHome";
 import HomeAboutMe from "./Components/HomeAboutMe";
+import HomeServices from "./Components/HomeServices";
 
 function Home() {
   return (
     <>
       <HomeHome />
       <HomeAboutMe />
+      <HomeServices />
     </>
   );
 }
