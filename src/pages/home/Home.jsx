04a1aac02a6536479f1aa560a7b028d4b264006a
Costa Ilisescu <coste_ilisescu@yahoo.com>
2023-09-30 15:13:35 +0100

    Add HomeOffers component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import HomeHome from "./Components/homeHome/HomeHome";
 import HomeAboutMe from "./Components/homeAboutMe/HomeAboutMe";
 import HomeServices from "./Components/homeServices/HomeServices";
-
+import HomeOffers from "./Components/homeOffers/HomeOffers";
 function Home() {
   return (
     <>
       <HomeHome />
       <HomeAboutMe />
       <HomeServices />
+      <HomeOffers />
     </>
   );
 }
