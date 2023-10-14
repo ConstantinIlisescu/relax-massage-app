b9065f7b34a3949d0a674cf248c9780384e21320
Costa Ilisescu <coste_ilisescu@yahoo.com>
2023-10-14 14:56:51 +0100

    Multiple changes - Home page complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import HomeHome from "./Components/homeHome/HomeHome";
-import HomeAboutMe from "./Components/homeAboutMe/HomeAboutMe";
-import HomeServices from "./Components/homeServices/HomeServices";
-import HomeOffers from "./Components/homeOffers/HomeOffers";
-import HomeReviews from "./Components/homeReviews/HomeReviews";
+import HomeHome from "./Sections/homeHome/HomeHome";
+import HomeAboutMe from "./Sections/homeAboutMe/HomeAboutMe";
+import HomeServices from "./Sections/homeServices/HomeServices";
+import HomeOffers from "./Sections/homeOffers/HomeOffers";
+import HomeReviews from "./Sections/homeReviews/HomeReviews";
+import HomeGift from "./Sections/homeGift/HomeGift.jsx";
+import HomeAffiliate from "./Sections/homeAffiliate/HomeAffiliate";
 function Home() {
   return (
     <>
@@ -12,6 +14,8 @@ function Home() {
       <HomeServices />
       <HomeOffers />
       <HomeReviews />
+      <HomeGift />
+      <HomeAffiliate />
     </>
   );
 }
---
+++ src/pages/home/Sections/homeGift/HomeGift.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./HomeGift.css";
+import Img from "../../../../images/gift.png";
+import Button from "../../../../components/button/Button";
+import "./homeGift.js";
+
+function HomeGift() {
+  return (
+    <section className="home-gift-section clip-path-polygon z-index-1">
+      <div className="home-gift-section-container z-index-2">
+        <div>
+          {/* <h2 className="gradient-light-text">Gift Voucher</h2> */}
+          <div className="home-gift-section-image">
+            <img src={Img} alt="..." />
+          </div>
+        </div>
+        <div className="home-gift-section-button">
+          {" "}
+          <Button
+            buttonStyle=""
+            linkTo="https://relaxmassagestudiocannock.setmore.com/"
+            toTarget="_blank"
+            isVisible={true}
+          >
+            Buy a Gift Voucher
+          </Button>
+        </div>
+      </div>
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default HomeGift;
