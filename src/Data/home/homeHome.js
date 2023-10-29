dc7bc85bc62b21911d95bdfed789377813f5df24
Costa Ilisescu <coste_ilisescu@yahoo.com>
2023-10-29 14:49:20 +0000

    update

--- src/Data/home/homeHome.js
+++ src/Data/home/homeHome.js
@@ -2,7 +2,7 @@ export const homeHome = {
   logoImage: "relaxLogo.png",
   logoImageDescription: "...",
   firstParagraph: "Welcome to Award Winning Massage Studio in Cannock",
-  mainTitle: ["Perfect", "mind-body", "harmony"],
+  mainTitle: ["PERFECT", "MIND-BODY", "HARMONY"],
   mainSubtitleParagraph: "Relax, lie back and let me take away your stress",
   mainImage: "main-img.png",
   mainImageDescription: "...",
