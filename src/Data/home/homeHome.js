4c13b0f80a5c154f1fad04138397d3c614384a2b
Costa Ilisescu <coste_ilisescu@yahoo.com>
2023-10-30 19:21:04 +0000

    Change home main page text and the text color

--- src/Data/home/homeHome.js
+++ src/Data/home/homeHome.js
@@ -2,7 +2,7 @@ export const homeHome = {
   logoImage: "relaxLogo.png",
   logoImageDescription: "...",
   firstParagraph: "Welcome to Award Winning Massage Studio in Cannock",
-  mainTitle: ["PERFECT", "MIND-BODY", "HARMONY"],
+  mainTitle: ["A SANCTUARY", "FOR YOUR", "WELLBEING"],
   mainSubtitleParagraph: "Relax, lie back and let me take away your stress",
   mainImage: "main-img.png",
   mainImageDescription: "...",
