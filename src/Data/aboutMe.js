f9a287c13413777e29778c2ebd9bd3bfa50945fd
Costa Ilisescu <coste_ilisescu@yahoo.com>
2023-11-05 14:11:06 +0000

    Updates

--- src/Data/aboutMe.js
+++ src/Data/aboutMe.js
@@ -1,10 +1,16 @@
 export const aboutMe = {
   title: "ABOUT ME",
   descriptionOne:
-    "Welcome to Relax Massage Studio Cannock, where your well-being is my top priority.I'm Aneta, a qualified and experienced massage therapist, and I'm here to help you unwind and find relief from the stresses of everyday life. Since opening my doors in January 2023, I have poured my heart and soul into creating a space that promotes relaxation and healing. With a passion for soft tissue manipulation and therapeutic pain relief, I am dedicated to providing personalised massages that cater to your specific needs. At my studio, I take a holistic approach to well-being. Each massage is carefully tailored to address your unique preferences and concerns, ensuring an unparalleled experience. My heated massage table, high quality skin products and serene ambiance create the perfect environment for relaxation.",
+    "Welcome to Relax Massage Studio Cannock, where your well-being is my top priority. I'm Aneta, a qualified and experienced massage therapist, and I'm here to help you unwind and find relief from the stresses of everyday life. Since opening my doors in January 2023, I have poured my heart and soul into creating a space that promotes relaxation and healing. With a passion for soft tissue manipulation and therapeutic pain relief, I am dedicated to providing personalised massages that cater to your specific needs.",
   imageOne: "about-me.jpg",
   imageOneDescription: "...",
   list: [
+    {
+      paragraph:
+        "At my studio, I take a holistic approach to well-being. Each massage is carefully tailored to address your unique preferences and concerns, ensuring an unparalleled experience. My heated massage table, high quality skin products and serene ambiance create the perfect environment for relaxation.",
+      image: "PHOTO-2023-11-05-13-59-42.jpg",
+      imageDescription: "...",
+    },
     {
       paragraph:
         "I am proud to share that I was recently recognised as the runner-up winner in the Best Massage Therapist category at the UK Beauty Awards 2023. This accolade is a testament to the dedication and commitment I bring to my craft.",
